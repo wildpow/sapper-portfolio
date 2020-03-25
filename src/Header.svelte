9f6350f352f5dfaa088f269c7073320d835044d9
wildpow <element1028@gmail.com>
2020-03-25 13:27:07 -0700

    working on header component

--- src/Header.svelte
+++ src/Header.svelte
@@ -4,28 +4,42 @@
 
 <style>
   @media screen and (max-width: 1920px) {
-    header {
+    .inner-wrapper {
       max-width: 1430px;
+      width: 92%;
     }
+  }
+  li:not(:last-child) {
+    margin-right: 50px;
+  }
+  nav ul :global(a) {
+    @apply text-2xl;
+  }
+  .brand :global(a) {
+    @apply text-4xl;
   }
 </style>
 
-<header class="flex justify-between bg-primary mx-auto">
-  <Link to="/">AARON WILDER</Link>
-  <nav>
-    <ul class="flex">
-      <li>
-        <Link to="/projects">Projects</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/services">Services</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    </ul>
-  </nav>
+<header class="bg-primary">
+  <div class="flex justify-between mx-auto inner-wrapper">
+    <div class="brand">
+      <Link class="text-6xl" to="/">AARON WILDER</Link>
+    </div>
+    <nav>
+      <ul class="flex">
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </header>
