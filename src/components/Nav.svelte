be38446277bd99a6189ab7c2bd20fb7495ade6cb
wildpow <element1028@gmail.com>
2020-04-06 21:15:49 -0700

    updates and hiding hamburge on large screens

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -53,7 +53,10 @@ header {
 <header class="absolute z-10 w-full" >
   <div style="border-color: rgba(255, 255, 255, 0.1)" class="flex items-center justify-between pt-5 pb-5 mx-auto border-b-2 border-solid md:pb-12 md:pt-16 inner-wrapper md:border-none" in:fly="{{ y: -100, duration: 1000, opacity: 0.1, easing: quintOut }}">
    <Brand bind:open {segment}/>
-    <Hamburger bind:open={open}/>
+   <div class=" lg:hidden">
+
+     <Hamburger bind:open={open}/>
+   </div>
     <nav class="hidden lg:block">
       <ul class="flex">
         <li>
