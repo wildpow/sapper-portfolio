a39142db96b415682f097dee8e572aad1684abeb
wildpow <element1028@gmail.com>
2020-04-17 19:56:35 -0700

    About page WIP

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -14,11 +14,21 @@
 <section
   class="pt-10 mx-auto sm:pt-20 md:pt-24"
   style="width: 92%; max-width: 925px;">
-  <div>
-    <h2>Who I Am</h2>
-    <p class="mt-4 leading-relaxed">
-      I'm a web developer based in Everett, WA.
-    </p>
+
+  <h2 class="text-5xl font-normal leading-tight ">Who I Am</h2>
+  <p class="mt-5 text-2xl font-light leading-relaxed lg:mt-8">
+    I'm a web developer based in Everett, WA.
+  </p>
+  <p>
+    After over ten years in the sales and customer service industries, I decided
+    to make a career change and pursue my dream of becoming a software engineer.
+    After completing Lambda School's nine-month computer science program and
+    subsequent three months paid team-lead role, I landed at a local mattress
+    company designing and implementing their JAMStack application.
+  </p>
+  <div class="mt-10">
+    <div />
+    <div />
   </div>
 </section>
 <Footer />
